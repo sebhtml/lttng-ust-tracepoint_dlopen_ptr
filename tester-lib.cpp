514af42661235b8bb07cdf1f1a6f1164484c3aea
Seb Boisvert <seb@boisvert.info>
2016-06-15 20:40:18 -0400

    enable tracepoint events in test lib
    Signed-off-by: Seb Boisvert <seb@boisvert.info>

--- tester-lib.cpp
+++ tester-lib.cpp
@@ -1,16 +1,16 @@
 
+#define TRACEPOINT_DEFINE
+
 #include "tracepoints.h"
 
 void
 test()
 {
-    /*
     tracepoint(
         gydle_om,
         alignQueryToTargetsTwoStrands_enter,
         "molecule-bar"
         );
-    */
 }
 
 
